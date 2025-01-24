125ca125a73fe784ff1c846578814ab766754c2d
H00Hong <1819642087@qq.com>
2025-01-24 17:09:43 +0800

    modified:   mywxwidgets/__init__.py

--- mywxwidgets/__init__.py
+++ mywxwidgets/__init__.py
@@ -3,6 +3,6 @@
 from . import dataview, grid, richtextbase, wxplot
 
 __all__ = ['dataview', 'grid', 'richtextbase', 'wxplot']
-version_info = (0, 1, 4)
-__version__ = '.'.join(map(str, version_info))
+__version_info__ = (0, 1, 4)
+__version__ = '.'.join(map(str, __version_info__))
 __updated__ = '2025-1-24'
